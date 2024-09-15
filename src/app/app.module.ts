f5db9263526babe126959edd0bd086aa170dd867
Oleg <mail.soloviov@gmail.com>
2024-09-15 20:52:55 +0200

    upgrate angular to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,35 +8,28 @@ import { CounterButtonComponent } from './components/counter-button.component/co
 import { CounterComponent } from './components/counter.component/counter.component';
 import { ActionFactory } from './services/state-store/action-factory';
 import { InventoryConnector } from './services/connectors/inventory.connector';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { InventoriesButtonComponent } from './components/inventories-button.component/inventories-button.component';
 import { InventoriesComponent } from './components/inventories.component/inventories.component';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxStateStoreModule } from 'ngx-state-store';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    CounterButtonComponent,
-    CounterComponent,
-    InventoriesButtonComponent,
-    InventoriesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    NgbModule,
-    HttpClientModule,
-    NgxStateStoreModule.forRoot({
-      storeName: 'store-demo',
-      log: environment.state.log,
-      timekeeping: environment.state.timekeeping,
-      initialState: AppInitialState
-    }),
-  ],
-  providers: [ActionFactory, InventoryConnector],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        CounterButtonComponent,
+        CounterComponent,
+        InventoriesButtonComponent,
+        InventoriesComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        FormsModule,
+        NgbModule,
+        NgxStateStoreModule.forRoot({
+            storeName: 'store-demo',
+            log: environment.state.log,
+            timekeeping: environment.state.timekeeping,
+            initialState: AppInitialState
+        })], providers: [ActionFactory, InventoryConnector, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule {
 }
